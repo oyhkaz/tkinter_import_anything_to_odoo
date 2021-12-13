4f890c6fe74a906a0380a018df7be3da519848ad
lasola <kazikazia012@gmail.com>
2021-12-13 18:43:07 +0800

    master

--- tkinter_learn.py
+++ tkinter_learn.py
@@ -1,8 +1,28 @@
 from tkinter import *
+from tkinter import filedialog
+
+class Application(Frame):
+    """file path load"""
+
+    def __init__(self, master = None):
+        super().__init__(master)
+        self.master = master 
+        self.pack()
+
+        self.pathfinder()
+    
+    def pathfinder(self):
+        self.button_pathload = Button(self)
+        self.button_pathload["text"] = "选择文件"
+        self.button_pathload.pack()
+        self.button_pathload["command"] = self.pathload 
+    def pathload(self):         
+        filedialog.askdirectory()
+
 root = Tk()
+root.title('my first GUI program')
+root.geometry('1500x1000+500+300')
 
-btn01 = Button(root,text='here')    #将按钮与窗口绑定
-
-btn01.pack()#标记位置
+app = Application(master = root)
 
 root.mainloop()
