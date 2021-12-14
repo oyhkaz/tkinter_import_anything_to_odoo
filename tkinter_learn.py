5f0b251d11ff65936b2ccc781bb2b1101069b13d
lasola <kazikazia012@gmail.com>
2021-12-14 12:37:45 +0800

    master

--- tkinter_learn.py
+++ tkinter_learn.py
@@ -1,9 +1,21 @@
+from sys import path
 import tkinter as tk
 from tkinter import filedialog
+from tkinter.constants import END
 
 class Application(tk.Frame):
     """file path load"""
+    def __init__(self,master = None):
+        super().__init__(master)
+        self.master = master 
+        self.pack()
+    
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
 
+class file_path_find(tk.Frame):
     def __init__(self, master = None):
         super().__init__(master)
         self.master = master 
@@ -19,15 +31,31 @@ class Application(tk.Frame):
         self.button_pathload["command"] = self.pathload 
     def pathload(self):         
         var = filedialog.askopenfilename()
-        self.text_path.insert('insert',var) 
+        self.text_path.delete(0,"end") 
+        self.text_path.insert('insert',var)       
     def pathtext(self):
         self.text_path = tk.Entry(self)
         self.text_path.pack(side='right')
 
+class date_show_excel(tk.Frame):
+    def __init__(self, master = None):
+        super().__init__(master)
+        self.master = master 
+        self.pack()
+
+        self.date_show_excel()
+
+    def date_show_excel(self):
+        for i in range(4):
+            for j in range(3):
+                label= tk.Label(self,text=1)
+                label.grid(column=i,row=j)
 root = tk.Tk()
 root.title('my first GUI program')
 root.geometry('1500x1000+500+300')
 
 app = Application(master = root)
+fpf = file_path_find(master = app)
+dse = date_show_excel(master = app)
 
 root.mainloop()
